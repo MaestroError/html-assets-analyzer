4c02e2f5042c72b9cb46fd52daa158f904c6ab3f
maestroerror <revaz.gh@gmail.com>
2023-10-27 13:08:31 +0400

    Scanner created

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <?php
+
+require "vendor/autoload.php";
+
+use maestroerror\Scanner;
+use maestroerror\FileManager;
+
+$scanner = new Scanner();
+
+$assets = $scanner->getAssets();
+$html = $scanner->getHtmlFiles();
+
+
+$matches = [];
+
+foreach ($html as $htmlFile) {
+    $htmlContent = file_get_contents("html\\".$htmlFile);
+    
+    foreach ($assets as $asset) {
+        $explode = explode("\\", $asset);
+        if (strpos($htmlContent, $explode[count($explode)-1]) !== false) {
+            $matches[$htmlFile][] = $asset;
+        }
+    }
+}
+
+// Sorting by HTML file path (if needed)
+ksort($matches);
+
+print_r($matches);
+echo json_encode($matches);
--- src/Scanner.php
+++ src/Scanner.php
@@ -1,2 +1,24 @@
 <?php
 
+namespace maestroerror;
+
+use maestroerror\FileManager;
+
+Class Scanner {
+
+    protected $assets;
+    protected $html;
+
+    public function __construct(string $assetsFolder = "assets", string $htmlFolder = "html") {
+        $this->assets = FileManager::folder($assetsFolder)->getFilesAll();
+        $this->html = FileManager::folder($htmlFolder)->getFilesAll();
+    }
+
+    public function getAssets() {
+        return $this->assets;
+    }
+
+    public function getHtmlFiles() {
+        return $this->html;
+    }
+}
